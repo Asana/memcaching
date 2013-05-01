d02aee785ca183149ffab9869b4c62736afc4f19
Isaac Wolkerstorfer <isaac@asana.com>
2013-05-01 19:07:48 +0200

    Add flush

--- test/integration.test.js
+++ test/integration.test.js
@@ -5,7 +5,11 @@ console.log("This test requires a running memcache server on port 11211")
 
 test("can talk to memcache", function(t) {
   var client = new MemcacheClient({ servers: {'127.0.0.1:11211': '10'} })
-  client.prefix = "prefix:"
+  client.flush(0, function(err, result) {
+    t.error(err, "should get no error")
+    t.equals(result, "OK", "should get result OK for a FLUSH")
+  })
+  client.prefix = "test:"
   client.set("foo", "bar", 3341, 0, function(err, result) {
     t.error(err, "should get no error")
     t.equals(result, "STORED", "should get result STORED for a SET")
@@ -14,10 +18,22 @@ test("can talk to memcache", function(t) {
     t.error(err, "should get no error")
     t.type(result, 'undefined', "should get result undefined for NOREPLY")
   })
-  client.mget(["foo", "bar"], function(err, response) {
+  client.mget(["foo", "bar"], function(err, result) {
     t.error(err, "should get no error")
-    t.equals(response.length, 1, "only get one response")
-    t.same(response[0].slice(0,3), ["foo", "baz", 1])
+    t.equals(result.length, 1, "only get one result")
+    t.same(result[0], ["foo", "baz", 1, undefined])
+  })
+  client.flush(0, function(err, result) {
+    t.error(err, "should get no error")
+    t.equals(result, "OK", "should get result OK for a FLUSH")
+  })
+  client.mget(["foo", "bar"], function(err, result) {
+    t.error(err, "should get no error")
+    t.equals(result.length, 0, "no results after flush")
+  })
+  client.set("foo", "baz", 3341, 1, true, function(err, result) {
+    t.error(err, "should get no error")
+    t.type(result, 'undefined', "should get result undefined for NOREPLY")
   })
   client.remove("foo", function(err, result) {
     t.error(err, "should get no error")
@@ -29,13 +45,17 @@ test("can talk to memcache", function(t) {
   })
   client.set("foo", "bär", 3341, 0, true, function(err, result) {
     t.error(err, "should get no error")
-    t.type(result, 'undefined', "should get no response on NOREPLY")
+    t.type(result, 'undefined', "should get no result on NOREPLY")
   })
-  client.get("foo", function(err, response) {
+  client.get("foo", function(err, result) {
     t.error(err, "should get no error")
-    t.equals(response[0], "bär", "should get the result as a string")
-    t.equals(response[1], 0, "should get the right flags")
-    t.end()
+    t.equals(result[0], "bär", "should get the result as a string")
+    t.equals(result[1], 0, "should get the right flags")
+  })
+  client.flush(0, function(err, result) {
+    t.error(err, "should get no error")
+    t.equals(result, "OK", "should get result OK for a FLUSH")
     client.close()
+    t.end()
   })
 })
