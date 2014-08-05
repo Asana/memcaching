564bef513247afea277f5683eea7e1577f4f51c9
Alex Cole <alexcole@asana.com>
2014-08-05 16:39:14 -0700

    Made a small syntax change and reverted style changes

--- test/test_server_distribution.js
+++ test/test_server_distribution.js
@@ -1,16 +1,16 @@
-module.exports = TestServerDistribution;
+module.exports = TestServerDistribution
 
 
 function TestServerDistribution(servers) {
-  this.servers = servers || [];
+  this.servers = servers || []
 }
 
-TestServerDistribution.prototype = {};
+TestServerDistribution.prototype = {}
 
 TestServerDistribution.prototype.add = function add(server) {
-  this.servers.push(server);
-};
+  this.servers.push(server)
+}
 
 TestServerDistribution.prototype.get = function get(key) {
-  return this.servers[0];
-};
+  return this.servers[0]
+}
