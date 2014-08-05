60ca47fc067f4ef800da107049916acd2302aa92
Alex Cole <alexcole@asana.com>
2014-08-05 15:48:07 -0700

    Add support for custom server distributions

---
+++ test/test_server_distribution.js
@@ -0,0 +1,16 @@
+module.exports = TestServerDistribution;
+
+
+function TestServerDistribution(servers) {
+  this.servers = servers || [];
+}
+
+TestServerDistribution.prototype = {};
+
+TestServerDistribution.prototype.add = function add(server) {
+  this.servers.push(server);
+};
+
+TestServerDistribution.prototype.get = function get(key) {
+  return this.servers[0];
+};
